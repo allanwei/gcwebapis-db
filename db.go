4c8aeb79764aabb0cd29fad9ac13be98b76d8f57
Allan Wei <AWei@smithandlong.com>
2018-10-03 23:43:31 -0400

    add db package

---
+++ db.go
@@ -0,0 +1,129 @@
+package db
+
+import (
+	"context"
+
+	"github.com/allanwei/gcwebapis-config"
+
+	//Add mssql driver
+	"database/sql"
+	//Add mssql driver
+	_ "github.com/denisenkom/go-mssqldb"
+	//Add mssql driver
+	//_ "github.com/jinzhu/gorm/dialects/mssql"
+)
+
+//DBcon ... Project DB type
+type DBcon struct {
+	DB    *sql.DB
+	Ctx   context.Context
+	Qstmt *sql.Stmt
+}
+
+//ExecuteResult ...
+type ExecuteResult struct {
+	LastInsertId int64 `json:"last_insert_id,omitempty"`
+	RowsAffected int64 `json:"rows_affected,omitempty"`
+}
+
+//ExecuteQuery ...
+func (c *DBcon) ExecuteQuery(sqlquery string) (*ExecuteResult, error) {
+	err := c.DB.PingContext(c.Ctx)
+	if err != nil {
+		return nil, err
+	}
+	c.Qstmt, err = c.DB.PrepareContext(c.Ctx, sqlquery)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Qstmt.Close()
+	r, err := c.Qstmt.ExecContext(c.Ctx)
+	if err != nil {
+		return nil, err
+	}
+	iid, err := r.LastInsertId()
+	if err != nil {
+		iid = 0
+	}
+	rc, err := r.RowsAffected()
+	if err != nil {
+		rc = 0
+	}
+	return &ExecuteResult{LastInsertId: iid, RowsAffected: rc}, nil
+}
+
+//GetResultRow ...
+func (c *DBcon) GetResultRow(sqlquery string) (*sql.Row, error) {
+	//defer c.DB.Close()
+
+	err := c.DB.PingContext(c.Ctx)
+	if err != nil {
+		return nil, err
+	}
+	c.Qstmt, err = c.DB.PrepareContext(c.Ctx, sqlquery)
+	if err != nil {
+		return nil, err
+	}
+
+	defer c.Qstmt.Close()
+	row := c.Qstmt.QueryRowContext(c.Ctx)
+	return row, nil
+
+}
+
+//GetResultRows ...
+func (c *DBcon) GetResultRows(sqlquery string) (*sql.Rows, error) {
+	//defer c.DB.Close()
+
+	err := c.DB.PingContext(c.Ctx)
+	if err != nil {
+		return nil, err
+	}
+	c.Qstmt, err = c.DB.PrepareContext(c.Ctx, sqlquery)
+	if err != nil {
+		return nil, err
+	}
+
+	defer c.Qstmt.Close()
+	rows, err := c.Qstmt.QueryContext(c.Ctx)
+	if err != nil {
+		return nil, err
+	}
+	return rows, nil
+
+}
+
+//CreateDBCon ... Create Project DB Connection
+func CreateDBCon(dbconnectionstring *string) (*DBcon, error) {
+	var cs string
+	var err error
+	if dbconnectionstring != nil {
+		cs = *dbconnectionstring
+	} else {
+		cs, err = config.GetDatabaseConf()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	db, err := sql.Open("mssql", cs)
+	if err != nil {
+		return nil, err
+	}
+	//Set db MaxIdleConns
+	//db.SetMaxIdleConns(10)
+	//Set db MaxOpenConns
+	//db.SetMaxOpenConns(100)
+	//defer db.Close()
+	// make sure connection is available
+	//ctx := context.Background()
+	//err = db.Ping(Context(ctx))
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+
+	}
+	c := DBcon{DB: db, Ctx: context.Background()}
+	return &c, nil
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/allanwei/gcwebapis-db
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/allanwei/gcwebapis-config v1.0.0 // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/micro/go-config v0.8.0 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+)
